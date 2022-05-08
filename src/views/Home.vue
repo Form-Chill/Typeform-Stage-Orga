84e5d828c3b5b986bc9b30a36fe2ead117ac412e
Juze0 <77743478+Juze0@users.noreply.github.com>
2022-05-08 15:52:48 +0200

    Ajout composants

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,29 @@
 <script>
+import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
     export default {
-
-    }
+    components: { Navbar, Footer }
+}
 
 </script>
 
 
 <template>
-    <nav class="navbar">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="../assets/logo.png" alt="">
-            </a>
-        </div>
+<Navbar></Navbar>
+    <div class="container">
+        <h1>Demandez l'avis de vos clients avec style.</h1>
+        <p>
+            Créez simplement des sondages polyvalents et accessibles à tous. Form&Chill est un formidable outil vous permettant de booster votre productivité.
+        </p>
+        <button class="btn">
+            Créer un sondage
+        </button>
 
-    </nav>
+        <img src="../assets/imgAccueil.jpg" alt="image Accueil"/>
+    </div>
+    
+    
+    <Footer></Footer>
 </template>
 
 <style>
