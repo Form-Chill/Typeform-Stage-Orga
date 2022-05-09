e02fdb73de7a69e5f7063b6d9d6f805840dced18
Rayan <44023290+MadShiro@users.noreply.github.com>
2022-05-09 12:16:16 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,14 @@
-<script>
-
-</script>
-
-
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light pt-3">
       <div class="container-fluid">
-      <img class="logo" src="./assets/logo.png">
+      <img class="logo pb-2" width="100" height="90" src="./assets/logo.png">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active text-light pb-3" aria-current="page" href="#">Sondages</a>
+              <a class="nav-link active text-light pb-3 ps-4" aria-current="page" href="#">Sondages</a>
             </li>
             <li class="nav-item">
               <a class="nav-link text-light" href="#">A propos</a>
@@ -26,16 +21,17 @@
         </div>
       </div>
     </nav>
-  
-</template>
-
-
+    
+    </template>
 <style>
+@font-face {
+  font-family: "Montserrat";
+  src: local("Montserrat"),   url(./assets/Montserrat-Medium.ttf) format("truetype");}
 
-
-.navbar{
+.navbar {
   background-color: #022C4F !important;
   font-family: "Montserrat";
+  font-size: large;
 }
 p {
   color: black;
