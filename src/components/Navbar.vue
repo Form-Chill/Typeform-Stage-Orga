80bfa7fc8a7bf1914fd8a5ef992cb6054c32860b
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2022-05-09 12:26:56 +0200

    minor fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light pt-3">
       <div class="container-fluid">
-      <img class="logo pb-2" width="100" height="90" src="./assets/logo.png">
+      <img class="logo pb-2" width="100" height="90" src="../assets/logo.png">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
