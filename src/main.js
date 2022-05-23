1f551f66714f7e64e81e0de3392b0d2e418a6e89
Rayan <rayanatrouni@gmail.com>
2022-05-23 10:54:38 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import router from './router/index.js'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
