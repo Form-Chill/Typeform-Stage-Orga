5e000f41f634fce4bab494eb2cd79b4461de0389
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2022-05-26 23:09:15 +0200

    Création de formulaire v2
    Restructuration de code pour alléger le composant Question et le rendre plus dynamique (et un peu plus joli mais bon ca reste pas bo :( )

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,40 +1,14 @@
 <template>
 
-<div id="apercu" class="card" style="width:1000px">
-    <div class="class-body row" >
+
+
+<div id="apercu" class="card">
+    <div class="class-body z row" >
+        <div class="col-md-12  bbbb">
         <p>{{this.enoncé}}</p>
+        </div>
         <component :is="type" :responses="responses"></component>
     </div>
-</div>
-
-
-<label>Texte enoncé:</label><br>  
-<input id="textenoncé" v-model="enoncé">
-<button  @click="" class="btn btn-success text-center"> Changer l'énoncé (WIP)</button>
-
-<div v-show= "type == 'MultipleChoice'">
-    <label>Texte radio bouton:</label><br>  
-    <input v-model="radio">
-    <button @click="addBtnRadio(radio)" class="btn btn-success text-center">Valider</button>
-</div>
-
-<!-- <div v-show= "tab == 'SimpleText'">
-    <label>Texte radio bouton:</label><br>  
-    <input v-model="radio">
-    <button @click="addBtnRadio(radio)" class="btn btn-success text-center">Valider</button>
-</div>
- -->
-
-
-    <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
-    Type de réponse
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <li><button @click="changeType('MultipleChoice')" class="dropdown-item" type="button">Choix multiple</button></li>
-    <li><button @click="changeType('SimpleText')" class="dropdown-item" type="button">Texte simple</button></li>
-    <li><button class="dropdown-item" type="button">(WIP)</button></li>
-  </ul>
 </div>
 
 </template>
@@ -43,40 +17,27 @@
 
 <script>
 import { ref } from 'vue'
-
 import RadioButton from "./Radiobutton.vue"
-
 import MultipleChoice from "./MultipleChoice.vue"
 
-
-
-
-
-export default{
+export default {
 components: { RadioButton,  MultipleChoice },
+
+props: {
+     enoncé: String,
+     type:String,
+     isActive: Boolean,
+     responses: Array
+},
 data(){
     return{
-        enoncé: ref(""),    
-        responses: ref([]),
         message: ref(""),
         radio: ref(""),
-        type: ref("")
     }
 
 },
 
 methods:{
-    addBtnRadio(s){
-        if(s.length == 0 )
-            alert("Le texte est vide");
-        else
-            this.responses.push(s);
-    },
-
-    changeType(type){
-        this.type = type;
-        this.responses.splice(0,this.responses.length)
-    }
 }
 
 }
@@ -100,20 +61,22 @@ background-image:none;
 } */
 
 #apercu{
-
-padding: 10%;
+    width: 80%;
+   
+    padding: 15% 30% 25% 10%;
+    margin: auto;
+    background-color: blanchedalmond;
 } 
 
 .class-body{
-  background-color: white !important;
+ 
   
 
 
 
 }
 
-p{
-
+.bbbb{
     overflow-wrap: break-word;
 }
 
--- src/views/PollCreate.vue
+++ src/views/PollCreate.vue
@@ -2,26 +2,68 @@
 <script>
 import Question from "../components/Question.vue"
 import Navbar from "../components/Navbar.vue"
+import { ref } from "vue";
 
 
  export default {
     components: { Question, Navbar },
     data(){
       return{
+        indexActive: 0,
         pollname: '',
-        questions: []
+
+        listQuestions: [{
+          enoncé: "",
+          type: "",
+          isActive: ref(true),
+          responses: []
+
+        }],
+
+        radio: ref("")
+        
       }
     },
     methods: {
+
       addQuestion(){
-        this.questions.push([]);
-      }
+        this.listQuestions.push({
+          enoncé: ref(""),
+          type: ref(""),
+          isActive: ref(false),
+          responses: []
+        });
+
+        this.currentQuestion(this.listQuestions.length - 1);
+      },
+
+      currentQuestion(index){
+        this.listQuestions[this.indexActive].isActive = false;
+        this.listQuestions[index].isActive = true;
+        this.indexActive = index;
+      },
+
+      test(){
+        console.log(this.indexActive);
+        console.log(this.listQuestions.length);
+      },
+      changeType(type){
+        this.listQuestions[this.indexActive].type = type;
+        this.listQuestions[this.indexActive].responses.splice(0,this.listQuestions[this.indexActive].responses.length)
+    },
+
+    addBtnRadio(s){
+        if(s.length == 0 )
+            alert("Le texte est vide");
+        else{
+            this.listQuestions[this.indexActive].responses.push(s);
+            this.radio = "";}
     }
 
 
 
 
-
+    }
   }
 
 
@@ -29,32 +71,43 @@ import Navbar from "../components/Navbar.vue"
 
 
 <template>
-<Navbar></Navbar>
+  <Navbar/>
+
   <h2 style="text-align:center">Créer un sondage </h2>
-   
-  <div class="container">
-
       <div class="row">
-        <div class="col-sm-12 col-md-12">
-        <label>Nom du sondage</label>  <br>     
-        <input v-model="radio">
-        <button class="btn btn-success">Valider (WIP)</button>
+        <div v-for="(item,index) in this.listQuestions">
+          <div v-if="item.isActive">
+          <Question v-bind="listQuestions[index]"></Question> 
+          
+          <label>Texte enoncé:</label><br>  
+          <input id="textenoncé" v-model="listQuestions[index].enoncé">
+          <button  @click="" class="btn btn-primary text-center"> Changer l'énoncé (WIP)</button>
+
+          <div v-show= "listQuestions[index].type == 'MultipleChoice'">
+            <label>Texte radio bouton:</label><br>  
+            <input v-model="radio">
+            <button @click="addBtnRadio(radio)" class="btn btn-success text-center">Valider</button>
+          </div>
+
+          <li><button @click="changeType('MultipleChoice')" class="dropdown-item" type="button">Choix multiple</button></li>
+          <li><button @click="changeType('SimpleText')" class="dropdown-item" type="button">Texte simple</button></li>
+          </div>
+
         </div>
-      
-
-    <br>     
-     <h2 style="text-align:center">Question </h2>
+      </div>
 
 
 
-      <Question></Question>
+      <div id="navigationQuestions" v-for="(item,index) in this.listQuestions" >
+        <button class="btn btn-primary" @click="currentQuestion(index)">  Question n° {{index}} </button>
     
-    </div>
+      </div>
 
-  </div>
+   <button @click="addQuestion()" class="btn btn-success">Ajouter une question</button>
 
+   <!-- <button @click="test()" class="btn btn-success">test</button> -->
 
-   <button @click="addQuestion()" class="btn btn-success">Ajouter une question (WIP)</button>
+   <Footer></Footer>
 
 </template>
 
@@ -62,10 +115,5 @@ import Navbar from "../components/Navbar.vue"
 
 <style>
 
-.class-body{
-  background-color: blue;
 
-
-
-}
 </style>
